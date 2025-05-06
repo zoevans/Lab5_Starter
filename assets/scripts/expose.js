a2374bb6a6db8883f08c2a7be5b7cf5f2cebfb98
Zoey Evans <zoevans@ucsd.edu>
2025-05-05 21:46:36 -0700

    Updated package.json file and implemented the horn select function.

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,25 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+
+  //Horn-select function
+  const selectElement = document.getElementById("horn-select");
+  const audioSelect = document.querySelector(".hidden");
+  const imageSelect = document.querySelector("img");
+
+
+  selectElement.addEventListener("change", (event) => {
+    if(event.target.value == "air-horn"){
+      audioSelect.src = "assets/audio/air-horn.mp3";
+      imageSelect.src = "assets/images/air-horn.svg";
+    } else if(event.target.value == "car-horn"){
+      audioSelect.src = "assets/audio/car-horn.mp3";
+      imageSelect.src = "assets/images/car-horn.svg";
+    } else {
+      audioSelect.src = "assets/audio/party-horn.mp3";
+      imageSelect.src = "assets/images/party-horn.svg";
+    }
+  });
+
 }
