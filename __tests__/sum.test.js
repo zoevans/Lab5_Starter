4a0df5f6afbae230cb998c5997710b1b222c86df
Zoey Evans <zoevans@ucsd.edu>
2025-05-06 19:44:45 -0700

    Fixed error in sum.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,7 +4,7 @@ import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
