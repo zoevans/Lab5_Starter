be785b2118437b97e7941052411b68241a067121
Zoey Evans <zoevans@ucsd.edu>
2025-05-06 18:02:57 -0700

    Updated sum.test.js file in accordance with the write-up to pass 2 tests.

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,13 @@
 // sum.test.js
 
+import { sum } from '../code-to-unit-test/sum';
+
 test('adds 1 + 2 to equal 3', () => {
   // TODO
+  expect(1 + 2).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  // TODO
+  expect(sum(1,2)).toBe(3);
 });
