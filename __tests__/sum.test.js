f5b7e3da9f9e71bb5f74d02664959e2553d45731
Zoey Evans <zoevans@ucsd.edu>
2025-05-06 18:35:52 -0700

    testing branch: should be wrong

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,7 +4,7 @@ import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(3);
+  expect(1 + 2).toBe(4);
 });
 
 test('adds 1 + 2 to equal 3', () => {
